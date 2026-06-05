6adeeb45a96c272f73727d804789701303d683d8
aryanxagarwal444 <aryanxagarwal444@gmail.com>
2026-06-05 12:58:27 +0530

    Day-5 uploaded

--- Day-5/perfect.cpp
+++ Day-5/perfect.cpp
@@ -5,6 +5,10 @@ int main(){
     int n;
     cout<<"Enter a number: ";
     cin>>n;
+    if(n<0){
+        cout<<"Invalid input";
+        exit(0);
+    }
     int s=0;
     for(int i=1; i<n; i++){
         if(n%i==0)
