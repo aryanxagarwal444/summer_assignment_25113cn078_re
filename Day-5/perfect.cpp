8e8ebe2ed7ec24873574501bb5d02671995bd039
aryanxagarwal444 <aryanxagarwal444@gmail.com>
2026-06-05 12:43:40 +0530

    Day-5 uploaded

---
+++ Day-5/perfect.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int s=0;
+    for(int i=1; i<n; i++){
+        if(n%i==0)
+        s+=i;
+    }
+    if(s==n)
+    cout<<"Perfect number";
+    else
+    cout<<"Not a perfect number";
+}
